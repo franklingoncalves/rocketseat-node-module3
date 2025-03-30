f690b141c766a41e79c5f914b84d2014a15df973
Franklin Gonçalves <franklingoncalves@gmail.com>
2025-03-29 21:44:04 -0300

    feat: create `UserAlreadyExistsError` for duplicate email handling

--- src/use-case/register.ts
+++ src/use-case/register.ts
@@ -1,6 +1,7 @@
 import { prisma } from "@/lib/prisma";
 import { Usersrepository } from "@/repositories/users-repository";
 import { hash } from "bcryptjs";
+import { UserAlreadyExistsError } from "./erros/user-already-exits-error";
 
 interface registerUseCaseRequest {
   name: string;
@@ -17,7 +18,7 @@ export class RegisterUseCase {
     const userWithSameEmail = await this.usersRepository.findByEmail(email);
 
     if (userWithSameEmail) {
-      throw new Error("E-mail already exists.");
+      throw new UserAlreadyExistsError();
     }
 
     await this.usersRepository.create({
