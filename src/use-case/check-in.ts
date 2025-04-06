ff45bf9ad91ed61abb115a83fcffc16c9d3f5d2b
Franklin Gonçalves <franklingoncalves@gmail.com>
2025-04-05 21:27:01 -0300

    feat: implement business rule to prevent multiple `check-ins` on the same day

--- src/use-case/check-in.ts
+++ src/use-case/check-in.ts
@@ -1,5 +1,7 @@
 import { CheckIn } from "@prisma/client";
 import { CheckInRepository } from "@/repositories/check-ins-repository";
+import dayjs from "dayjs";
+import { date } from "zod";
 
 interface CheckInUseCaseRequest {
   userId: string;
@@ -17,6 +19,18 @@ export class CheckInUseCase {
     userId,
     gymId,
   }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const startOfTheDay = dayjs(date).startOf("date");
+    const endOfTheDay = dayjs(date).endOf("date");
+
+    const checkInOnSameDate = await this.checkInsRepository.findByUserIdOnDate(
+      userId,
+      new Date()
+    );
+
+    if (checkInOnSameDate) {
+      throw new Error();
+    }
+
     const checkIn = await this.checkInsRepository.create({
       gym_id: gymId,
       user_id: userId,
