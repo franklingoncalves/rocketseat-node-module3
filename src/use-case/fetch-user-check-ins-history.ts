ab844b853e7e08caaf9378b382609f88bd0d2bcc
Franklin Gonçalves <franklingoncalves@gmail.com>
2025-04-05 23:15:02 -0300

    feat: create use case for `fetching user check-in history`

---
+++ src/use-case/fetch-user-check-ins-history.ts
@@ -0,0 +1,28 @@
+import { CheckIn } from "@prisma/client";
+import { CheckInRepository } from "@/repositories/check-ins-repository";
+
+interface FetchUserCheckInsHistoryUseCaseRequest {
+  userId: string;
+}
+
+interface FetchUserCheckInsHistoryUseCaseResponse {
+  checkIns: CheckIn[];
+}
+
+export class FetchUserCheckInsHistoryUseCase {
+  constructor(private checkInsRepository: CheckInRepository) {}
+
+  async execute({
+    userId,
+    page,
+  }: FetchUserCheckInsHistoryUseCaseRequest): Promise<FetchUserCheckInsHistoryUseCaseResponse> {
+    const checkIns = await this.checkInsRepository.findManyByUserId(
+      userId,
+      page
+    );
+
+    return {
+      checkIns,
+    };
+  }
+}
