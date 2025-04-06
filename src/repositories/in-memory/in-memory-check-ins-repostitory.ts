57b6c1b725128745bdbf8c4b3bf53b182ad1f4a0
Franklin Gonçalves <franklingoncalves@gmail.com>
2025-04-05 23:14:01 -0300

    feat: update `findManyByUserId` implementation in `InMemoryCheckInsRepository` to support pagination

--- src/repositories/in-memory/in-memory-check-ins-repostitory.ts
+++ src/repositories/in-memory/in-memory-check-ins-repostitory.ts
@@ -38,7 +38,9 @@ export class InMemoryCheckInsRepository implements CheckInRepository {
     return checkInOnSameDate;
   }
 
-  async findManyByUserId(userId: string) {
-    return this.items.filter((item) => item.user_id === userId);
+  async findManyByUserId(userId: string, page: number) {
+    return this.items
+      .filter((item) => item.user_id === userId)
+      .slice((page - 1) * 20, page * 20);
   }
 }
