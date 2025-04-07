9f4c14dbf2a2e32d0b38a4d2f23013e33304e734
Franklin Gonçalves <franklingoncalves@gmail.com>
2025-04-07 10:56:28 -0300

    fix: update check-in validation to allow validation within 20 minutes of creation

--- src/use-case/validate-check-in.spec.ts
+++ src/use-case/validate-check-in.spec.ts
@@ -11,11 +11,11 @@ describe("Validate Check-In Use Case", () => {
     checkInRepository = new InMemoryCheckInsRepository();
     sut = new ValidateCheckInUseCase(checkInRepository);
 
-    // vi.useFakeTimers();
+    vi.useFakeTimers();
   });
 
   afterEach(() => {
-    // vi.useRealTimers();
+    vi.useRealTimers();
   });
 
   it("should be able to validate check-in", async () => {
@@ -38,5 +38,24 @@ describe("Validate Check-In Use Case", () => {
         checkInId: "inexistent-check-in-id",
       })
     ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it("should not be able to validate the check-in after 20 minutes of its creation", async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40));
+
+    const createdCheckIn = await checkInRepository.create({
+      gym_id: "gym-01",
+      user_id: "user-01",
+    });
+
+    const twentyOneMinutesInMs = 1000 * 60 * 21;
+
+    vi.advanceTimersByTime(twentyOneMinutesInMs);
+
+    await expect(() =>
+      sut.execute({
+        checkInId: createdCheckIn.id,
+      })
+    ).rejects.toBeInstanceOf(Error);
   });
 });
