ebfbde8a57ff81eb0f5d8f0ee5f312ddd0505dcd
Franklin Gonçalves <franklingoncalves@gmail.com>
2025-05-03 17:39:11 -0300

    refactor: update `profile.spec.ts` to use new user creation and authentication utility

--- src/http/controllers/users/profile.spec.ts
+++ src/http/controllers/users/profile.spec.ts
@@ -3,6 +3,7 @@ import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
 import { app } from '@/app'
 import exp from 'constants'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
 
 describe('Profile (e2e)', () => {
 	beforeAll(async () => {
@@ -14,18 +15,7 @@ describe('Profile (e2e)', () => {
 	})
 
 	it('should be able to get user profile', async () => {
-		await request(app.server).post('/users').send({
-			name: 'John Doe',
-			email: 'johndoe@example.com',
-			password: '123456',
-		})
-
-		const authResponse = await request(app.server).post('/sessions').send({
-			email: 'johndoe@example.com',
-			password: '123456',
-		})
-
-        const { token } = authResponse.body
+        const { token } = await createAndAuthenticateUser(app)
 
 		const profileResponse = await request(app.server)
             .get('/me')
