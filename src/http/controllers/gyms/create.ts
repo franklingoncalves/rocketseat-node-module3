21c0cf47b56527c50ba60feb3a8dd3943e85bc97
Franklin Gonçalves <franklingoncalves@gmail.com>
2025-05-03 16:30:39 -0300

    feat: add HTTP handler for creating gyms

---
+++ src/http/controllers/gyms/create.ts
@@ -0,0 +1,34 @@
+import { makeCreateGymUseCase } from '@/use-case/factories/make-create-gym-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function create(request: FastifyRequest, reply: FastifyReply) {
+	const createGymBodySchema = z.object({
+		title: z.string(),
+        name: z.string(),
+		description: z.string().nullable(),
+		phone: z.string().nullable(),
+		latitude: z.number().refine((value) => {
+			return Math.abs(value) <= 90
+		}),
+		longitude: z.number().refine((value) => {
+			return Math.abs(value) <= 180
+		}),
+	})
+
+	const { title, description, latitude, longitude, name, phone } =
+		createGymBodySchema.parse(request.body)
+
+	const registerUseCase = makeCreateGymUseCase()
+
+	await registerUseCase.execute({
+		title,
+        description,
+		latitude,
+		longitude,
+		name,
+		phone,
+	})
+
+	reply.status(201).send()
+}
