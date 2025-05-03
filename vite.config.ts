d45b1aab23c8acd0af0b331aadd339f848cdfee8
Franklin Gonçalves <franklingoncalves@gmail.com>
2025-05-03 14:49:37 -0300

    chore: configure Vitest environment for e2e testing

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,25 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    dir: 'src',
+    workspace: [
+      {
+        extends: true,
+        test: {
+          name: 'unit',
+          dir: 'src/use-case'
+        }
+      },
+      {
+        extends: true,
+        test: {
+          name: 'e2e',
+          dir: 'src/http/controllers',
+          environment: 
+            './prisma/vitest-environment-prisma/schema-test-environment.ts',
+        }
+      }
+    ]
+  }
 });
